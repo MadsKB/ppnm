45fa7d894b7d2d9707bffdb964ea683fa46a7bec
Mads KB <mads.krogh.b@gmail.com>
2023-02-08 09:25:58 +0100

    Lecture stuff

--- Exercises/math/main.cs
+++ Exercises/math/main.cs
@@ -1,12 +1,14 @@
 using System;
-
-class math {
-	static void Main(){
+using static System.Console;
+using static System.Math;
+public static class math {
+	public static void Main(){
 		double sqrt2 = Math.Sqrt(2.0);
-		Console.Write($"sqrt(2)={sqrt2}\n");
-		Console.Write($"sqrt2*sqrt2={sqrt2*sqrt2} (should be equal to 2)\n");
+		Write($"sqrt(2)={sqrt2}\n");
+		Write($"sqrt2*sqrt2={sqrt2*sqrt2} (should be equal to 2)\n");
 		
-		double expPi = Math.Exp(PI)
-		Consol.Write(
+		double expPi = Math.Exp(PI);
+		Write($"exp(Pi)={expPi}\n");
+		double 
 	}	
 }
---
+++ lecture stuff/static_hello.cs
@@ -0,0 +1,4 @@
+public static class static_hello{
+	public static string greeting = "hello\n";
+	public static void print(){ System.Console.WriteLine(greeting); }
+}
